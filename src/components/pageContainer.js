f3963985d22ecdbccc1635f38f3099cb60269b5f
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-04 15:18:05 -0800

    Added pageContainer to switch pages on nav

---
+++ src/components/pageContainer.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import NavBar from "./navbar";
+import About from "./aboutMe";
+import Contact from "./contact";
+import Portfolio from "./portfolio";
+import Resume from "./resume";
