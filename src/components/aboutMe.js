dbae08d4159613108f5c97c666f2d4321ccdd592
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-04 15:21:24 -0800

    Added about me page contents

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,7 +3,14 @@ import Navbar from './navbar'
 
 const About = () => {
     return (
-        <About />
+        <div className="aboutMe">
+            <h1>About Me</h1>
+            <p>
+                Current student at UCSD Extension program for Full Stack Web Development. 
+                Known for being a hard worker and a good problem solver. 
+                Able to visualize the outcome of a project to get a better understanding of what needs to be done.
+            </p>
+        </div>
     );
 };
 
