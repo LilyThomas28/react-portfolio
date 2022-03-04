9a94f4cedc9a106277c83b2db13ddee36555163b
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-04 15:43:10 -0800

    Added navbar contents

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Navbar = ({ currentPage, handlePageChange }) => {
     return (
         <header>
             <h1>Lily Thomas</h1>
             <nav>
-                
+                        <ul className="nav nav-tabs d-flex justify-content-end">
+                    <li className="nav-item">
+                        <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                            About
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                            Portfolio
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
+                            Resume
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                            Contact
+                        </a>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
