99845773a05282f6be6af323f94c483bd871b1ed
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-04 15:45:34 -0800

    Added resume Link

--- src/components/resume.js
+++ src/components/resume.js
@@ -4,6 +4,7 @@ const Resume = () => {
     return (
         <div>
             <h1>Resume</h1>
+            <a href="https://docs.google.com/document/d/1r8t9l68xNm_s4vJxsL8kCCYZyo_ErEcf9IE1gR3k1VU/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume Link</a>
         </div>
     );
 };
